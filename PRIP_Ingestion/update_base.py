714e64af65890cef5722652ab5bfe421c970b535
EO User <eouser@adg-rpp.novalocal>
2024-01-26 14:40:37 +0000

    separated upload of json in a funciton to work both on single files and on list

--- PRIP_Ingestion/update_base.py
+++ PRIP_Ingestion/update_base.py
@@ -4,13 +4,18 @@ import glob
 import json
 import time
 import os
+import sys
 import requests
 from ingestion.lib.auxip import get_token_info
 
 odata_datetime_format = "%Y-%m-%dT%H:%M:%SZ[GMT]"
 odata_datetime_nosec_format = "%Y-%m-%dT%H:%MZ[GMT]"
-url_dev = "https://dev.reprocessing-preparation.ml/reprocessing.svc"
-url_prod = "https://reprocessing-auxiliary.copernicus.eu/reprocessing.svc"
+
+def get_reprobase_base_endpoint(mode):
+    base_endpoint = "https://dev.reprocessing-preparation.ml/reprocessing.svc"
+    if mode == 'prod':
+        base_endpoint = "https://reprocessing-auxiliary.copernicus.eu/reprocessing.svc"
+    return base_endpoint
 
 def send_request(request, jsonload, access_token):
     headers = {'Content-type': 'application/json','Authorization' : 'Bearer %s' % access_token}
@@ -34,6 +39,24 @@ def send_request(request, jsonload, access_token):
         resp.close()
     return res
 
+def upload_json(json_file, base_url, token_info):
+    result = True
+    with open(json_file) as f:
+        json_base = json.load(f)
+    if "Id" in json_base:
+        request = base_url + "/AuxFiles(" + json_base["Id"]+")"
+    else:
+        request = base_url + "/AuxTypes('" + json_base["LongName"]+"')"
+    print(request)
+    try:
+        result = send_request(request, json_base, token_info['access_token'])
+        print(result)
+    except Exception as e:
+        print(e)
+        print("Le fichier {0} n'a pas été uploadé sur reprobase".format(json_file))
+        result = False
+    print("Success: ", result)
+    return result
 
 def main():
     parser = argparse.ArgumentParser(description="",  # main description for help
@@ -58,21 +81,11 @@ def main():
 
     fileUploadError=[]
 
-    if args.mode == "dev":
-        url = url_dev
-    else:
-        url = url_prod
+    url = get_reprobase_base_endpoint(args.mode)
         
     if os.path.isfile(args.input):
-        with open(args.input) as f:
-            json_base = json.load(f)
-        if "Id" in json_base:
-            request = url + "/AuxFiles(" + json_base["Id"]+")"
-        else:
-            request = url + "/AuxTypes('" + json_base["LongName"]+"')"
-        print(request)
-        result = send_request(request,json_base,token_info['access_token'])
-        print(result)
+        if not upload_json(args.input, url, token_info):
+            fileUploadError.append(args.input)
     else:
         timer_start = time.time()
         for j in glob.glob(os.path.join(args.input,"*.json")):
@@ -80,25 +93,16 @@ def main():
             timer_stop = time.time()
             elapsed_seconds = timer_stop - timer_start
             if elapsed_seconds > 300 :
+                timer_start = time.time()
                 token_info = get_token_info(args.user, args.password, mode=args.mode)
-            with open(j) as f:
-                json_base = json.load(f)
-            if "Id" in json_base:
-                request = url + "/AuxFiles(" + json_base["Id"] + ")"
-            else:
-                request = url + "/AuxTypes('" + json_base["LongName"] + "')"
-            print(request)
-            try:
-                result = send_request(request, json_base, token_info['access_token'])
-                print(result)
-            except Exception as e:
-                print(e)
-                print("Le fichier {0} n'a pas été uploadé sur reprobase".format(j))
-                fileUploadError.append(j)
+            if not upload_json(j, url, token_info):
+                fileUploaderror.append(j)
     if len(fileUploadError) > 0:
         print("Une erreur s'est produite pour l'upload des fichiers suivants :")       
         for f in fileUploadError:
             print(f)
+        sys.exit(1)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
