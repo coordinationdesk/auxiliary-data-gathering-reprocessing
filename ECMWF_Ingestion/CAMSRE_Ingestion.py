33008647835a032ad1b0d384f60535a8fb845e34
EO User <eouser@adg-rpp.novalocal>
2024-03-29 10:37:28 +0000

    Moved hdr creation function to separate module (in order to later reuse for camsan products)

--- ECMWF_Ingestion/CAMSRE_Ingestion.py
+++ ECMWF_Ingestion/CAMSRE_Ingestion.py
@@ -2,25 +2,7 @@ import os,re,subprocess,datetime,glob,shutil,argparse
 import sys
 from lxml import etree as ET
 import DownloadCamsreGrib
-
-def create_hdr(cams_header_xml,
-               current_date, validity_start, validity_stop,
-               hdr_filename,
-	       cams_ns,
-               xml_time_format):
-    #
-    # Create HDR file
-    #
-    valid_start_xml = validity_start.strftime(xml_time_format)
-    valid_stop_xml = validity_stop.strftime(xml_time_format)
-    crea_time_xml = current_date.strftime(xml_time_format)
-    cams_header_xml.find("File_Name", cams_ns).text = hdr_filename
-    val_period_element = cams_header_xml.find("Validity_Period", cams_ns)
-    val_period_element.find("Validity_Start",cams_ns).text = valid_start_xml
-    val_period_element.find("Validity_Stop", cams_ns).text = valid_stop_xml
-    cams_header_xml.find("Source", cams_ns).find("Creation_Date", cams_ns).text = valid_stop_xml
-    print(fixed_header_hdr_cams.find("File_Name",cams_ns).text)
-    print(val_period_element.find("Validity_Start",cams_ns).text)
+from ecmwf_product import create_hdr
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This script generates the CAMSRE files for a period",  # main description for help
---
+++ ECMWF_Ingestion/ecmwf_product.py
@@ -0,0 +1,20 @@
+def create_hdr(cams_header_xml,
+               current_date, validity_start, validity_stop,
+               hdr_filename,
+	       cams_ns,
+               xml_time_format):
+    #
+    # Create HDR file
+    #
+    valid_start_xml = validity_start.strftime(xml_time_format)
+    valid_stop_xml = validity_stop.strftime(xml_time_format)
+    crea_time_xml = current_date.strftime(xml_time_format)
+    cams_header_xml.find("File_Name", cams_ns).text = hdr_filename
+    val_period_element = cams_header_xml.find("Validity_Period", cams_ns)
+    val_period_element.find("Validity_Start",cams_ns).text = valid_start_xml
+    val_period_element.find("Validity_Stop", cams_ns).text = valid_stop_xml
+    cams_header_xml.find("Source", cams_ns).find("Creation_Date", cams_ns).text = valid_stop_xml
+    print(fixed_header_hdr_cams.find("File_Name",cams_ns).text)
+    print(val_period_element.find("Validity_Start",cams_ns).text)
+
+
