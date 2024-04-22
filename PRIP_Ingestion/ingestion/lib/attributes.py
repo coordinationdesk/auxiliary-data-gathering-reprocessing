88cf3ff8474f7a1e1c2b6b4bf1eadb99c04c935f
EO User <eouser@adg-rpp.novalocal>
2024-04-22 16:08:58 +0000

    Removed extra print Fixed argument names added function to uncompress zip file Fixed list of types to be handled with EOF for S2

--- PRIP_Ingestion/ingestion/lib/attributes.py
+++ PRIP_Ingestion/ingestion/lib/attributes.py
@@ -14,25 +14,31 @@ import sys
 import xml.etree.ElementTree as ET
 
 def getValueByName(root_node,attribute_name):
+    if root_node is None:
+        raise Exception("No node passed to retrieve attribute {} value".format(attribute_name))
     for elt in root_node:
         if attribute_name in elt.tag:
             return elt.text
-    
+
     return None
 
 
 def getNodeByName(root_node,node_name):
+    if root_node is None:
+        raise Exception("No node passed to retrieve node with name {} ".format(attribute_name))
     for elt in root_node:
         if node_name in elt.tag:
             return elt
-    
+
     return None
 
 def getNodeByID(metadat_section,ID):
+    if metadat_section is None:
+        raise Exception("No section passed to retrieve node with ID {} ".format(ID))
     for elt in metadat_section:
         if ID == elt.get('ID'):
             return elt
-    
+
     return None
 
 
@@ -44,6 +50,26 @@ def md5(fname):
     return hash_md5.hexdigest()
 
 odata_datetime_format = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+def _get_EOF_file_attributes(eof_file_path):
+    # TODO: Check if xml file is reachable for parse command
+    tree = ET.parse(eof_file_path)
+    fixed_header = tree.getroot()[0][0]
+    source_node = getNodeByName(fixed_header, 'Source')
+    validity_period = getNodeByName(fixed_header, 'Validity_Period')
+    attributes = {
+        "productType": getValueByName(fixed_header, 'File_Type'),
+        "platformShortName": getValueByName(fixed_header, 'Mission'),
+        "platformSerialIdentifier": getValueByName(fixed_header, 'Mission'),
+        "processingDate": getValueByName(source_node, 'Creation_Date').split('UTC=')[1],
+        "beginningDateTime": getValueByName(validity_period, 'Validity_Start').split('UTC=')[1],
+        "endingDateTime": getValueByName(validity_period, 'Validity_Stop').split('UTC=')[1],
+        "processingCenter": getValueByName(source_node, 'System'),
+        "processorName": getValueByName(source_node, 'Creator'),
+        "processorVersion": getValueByName(source_node, 'Creator_Version')
+    }
+    return attributes
+
 
 def _get_S1_file_attributes(file_path):
     filename_zip = os.path.basename(file_path)
@@ -70,22 +96,29 @@ def _get_S1_file_attributes(file_path):
         except Exception as e:
             print(e)
 
+        print("Extracting metadata attributes for ", filename_zip)
         tree = ET.parse(xml_file)
         root = tree.getroot()
 
         metadataSection = getNodeByName(root, 'metadataSection')
-
-        processing = getNodeByID(metadataSection, 'processing')
-        platform = getNodeByID(metadataSection, 'platform')
+        print("Checking if Metadata are present in manifest file")
+        if metadataSection is None:
+            raise Exception("Metadata Section missing in SAFE manifest file: could not extract product attributes")
+        processing_node = getNodeByID(metadataSection, 'processing')
+        platform_node = getNodeByID(metadataSection, 'platform')
 
         generalProductInformation = getNodeByID(metadataSection, 'generalProductInformation')
         if generalProductInformation is None:  # for AUX_PP1,AUX_PP2,AUX_CAL,AUX_INS
+            # print("Getting Standalone  Product Information node ")
             generalProductInformation = getNodeByID(metadataSection, 'standAloneProductInformation')
 
-        processing = getNodeByName(getNodeByName(processing, 'metadataWrap'), 'xmlData')
+        processing_metadata_container = getNodeByName(getNodeByName(processing_node, 'metadataWrap'), 'xmlData')
+        processing_metadata = processing_metadata_container[0][0]
+        print("Identified main nodes; reading attributes")
         generalProductInformation = getNodeByName(getNodeByName(generalProductInformation, 'metadataWrap'), 'xmlData')
-        platform = getNodeByName(getNodeByName(platform, 'metadataWrap'), 'xmlData')
+        platform_metadata = getNodeByName(getNodeByName(platform_node, 'metadataWrap'), 'xmlData') if platform_node is not None else None
 
+        # print("Getting Begin validity from General Product Information")
         beginningDateTime = getValueByName(generalProductInformation[0], 'validity')
         product_type = getValueByName(generalProductInformation[0], 'auxProductType')
 
@@ -98,19 +131,17 @@ def _get_S1_file_attributes(file_path):
 
         attributes = {
             "productType": product_type,
-            "platformShortName": getValueByName(platform[0], 'familyName'),
-            "platformSerialIdentifier": getValueByName(platform[0], 'number'),
-            "processingDate": processing[0].get('start'),
+            "processingDate": processing_metadata_container[0].get('start'),
             "beginningDateTime": beginningDateTime,
             "endingDateTime": endingDateTime,
-            "processingCenter": processing[0][0].get('site'),
+            "processingCenter": processing_metadata.get('site'),
         }
 
         # try to get processorName / processorVersion
         # these attributes are missing in some .SAFE files ( may be are missing in all of them )
         try:
-            processorName = getNodeByName(processing[0][0], 'software').get('name')
-            processorVersion = getNodeByName(processing[0][0], 'software').get('version')
+            processorName = getNodeByName(processing_metadata, 'software').get('name')
+            processorVersion = getNodeByName(processing_metadata, 'software').get('version')
 
             attributes["processorName"] = processorName
             # BUG: FIXED : processorName was assigned to attribute processorVersion!
@@ -118,6 +149,11 @@ def _get_S1_file_attributes(file_path):
 
         except Exception as e:
             pass
+        if platform_metadata is not None:
+            attributes.update({
+            "platformShortName": getValueByName(platform_metadata[0], 'familyName'),
+            "platformSerialIdentifier": getValueByName(platform_metadata[0], 'number'),
+            })
 
     # ==========================================================================================
     #                      S1 .EOF FILES
@@ -127,23 +163,7 @@ def _get_S1_file_attributes(file_path):
         unzip_command = "unzip %s" % file_path
 
         os.system(unzip_command)
-        # TODO: Check if xml file is reachable for parse command
-        tree = ET.parse(xml_file)
-        fixed_header = tree.getroot()[0][0]
-        source_node = getNodeByName(fixed_header, 'Source')
-        validity_period = getNodeByName(fixed_header, 'Validity_Period')
-        attributes = {
-            "productType": getValueByName(fixed_header, 'File_Type'),
-            "platformShortName": getValueByName(fixed_header, 'Mission'),
-            "platformSerialIdentifier": getValueByName(fixed_header, 'Mission'),
-            "processingDate": getValueByName(source_node, 'Creation_Date').split('UTC=')[1],
-            "beginningDateTime": getValueByName(validity_period, 'Validity_Start').split('UTC=')[1],
-            "endingDateTime": getValueByName(validity_period, 'Validity_Stop').split('UTC=')[1],
-            "processingCenter": getValueByName(source_node, 'System'),
-            "processorName": getValueByName(source_node, 'Creator'),
-            "processorVersion": getValueByName(source_node, 'Creator_Version')
-        }
-
+        attributes = _get_EOF_file_attributes(xml_file)
     # with open(os.path.join(filename_zip + ".json"), 'w') as json_file:
     #     json.dump(attributes, json_file)
 
@@ -152,23 +172,27 @@ def _get_S1_file_attributes(file_path):
     return attributes
 
 
-def _get_S2_file_attributes(file_path):
+def _extract_hdr_from_product(file_path):
     filename_zip = os.path.basename(file_path)
     filename = filename_zip.split('.')[0]
     product_type = filename[9:19]
+    # extract to separate function (hdr_file, product_type, filename)
+    # PARSE extracted uncompressed file (XML File)
     # UNCOMPRESS HDR file (Tar gzip)
-    hdr_file = "%s.HDR" % filename
+    hdr_filename = "%s.HDR" % filename
     if product_type in ['AUX_ECMWFD', 'AUX_UT1UTC']:
-        tar_process = subprocess.run(["tar", "xzf", file_path, hdr_file])
+        tar_process = subprocess.run(["tar", "xzf", file_path, hdr_filename])
         if tar_process.returncode != 0:
             hdr_file = "%s/%s.HDR" % (filename, filename)
-            tar_process = subprocess.run(["tar", "xzf", file_path, hdr_file])
+            tar_process = subprocess.run(["tar", "xzf", file_path, hdr_filename])
             if tar_process.returncode != 0:
                 raise Exception("Impossible to get the HDR file in the file " + filename)
     else:
-        tar_command = "tar xzf %s %s" % (file_path, hdr_file)
+        tar_command = "tar xzf %s %s" % (file_path, hdr_filename)
         os.system(tar_command)
-    # PARSE extracted uncompressed file (XML File)
+    return filename, hdr_filename
+    
+def _get_hdr_file_attributes(hdr_file):
     tree = ET.parse(hdr_file)
     root = tree.getroot()
     if root.tag == 'Earth_Explorer_File':  # pug in S2 Files
@@ -192,18 +216,75 @@ def _get_S2_file_attributes(file_path):
         "processorName": getValueByName(source_node, 'Creator'),
         "processorVersion": getValueByName(source_node, 'Creator_Version')
     }
-    # with open(os.path.join(filename_zip + ".json"), 'w') as json_file:
-    #     json.dump(attributes, json_file)
-    os.remove(hdr_file)
-    shutil.rmtree(filename, ignore_errors=True)
     return attributes
 
-def _get_S3_file_attributes(file_path):
+def _uncompress(compressed_filepath):
+    if '.zip' in file_path:
+        filename_zip = os.path.basename(compressed_filepath)
+        uncompressed_filename = filename_zip.split('.zip')[0]
+        unzip_command = "unzip %s" % compressed_filepath
+        print("Uncompressed file name: ", uncompressed_filename)
+        print("Zip command: ", unzip_command)
+        try:
+            os.system(unzip_command)
+        except Exception as e:
+            print(e)
+    return uncompressed_filename
+
+def _get_S2_file_attributes(file_path):
+    # PARSE extracted uncompressed file (XML File)
+    basename = os.path.basename(file_path)
+    product_type = basename[9:19]
+    print("Getting attributes for file of type ", product_type)
+    if product_type in ['MPL_ORBRES', 'MPL_ORBPRE', 'REP__SUP__']:
+        print("Extracting attributes for EOF file")
+        # TODO CHeck extension if EOF
+        # 
+        if '.zip' in file_path:
+            filename_zip = os.path.basename(file_path)
+            eof_filename = filename_zip.split('.zip')[0]
+            unzip_command = "unzip %s" % file_path
+            print("Uncompressed file name: ", eof_filename)
+            print("Zip command: ", unzip_command)
+            try:
+                os.system(unzip_command)
+            except Exception as e:
+                print(e)
+        else:
+            print("EOF file not compressed ")
+            # File not compressed, take as it is
+            eof_filename = file_path
+        attributes = _get_EOF_file_attributes(eof_filename)
+        if '.zip' in file_path:
+            os.remove(eof_filename)
+    eilf product_type in ['MANPRE']:
+        print("Extracting attributes for MANeuver file")
+        prod_filename = _uncompress(file_path)
+        attributes = _get_MANPRE_file_attributes(prod_filename)
+        os.remove(prod_filename)
+    else:
+        print("Extracting attributes from HDR file inside compress tar")
+        product_name, hdr_file = _extract_hdr_from_product(file_path)
+        attributes = _get_hdr_file_attributes(hdr_file)
+        os.remove(hdr_file)
+        shutil.rmtree(product_name, ignore_errors=True)
+    # with open(os.path.join(filename_zip + ".json"), 'w') as json_file:
+    #     json.dump(attributes, json_file)
+    return attributes
+
+def _get_zip_manifest(file_path):
     filename_zip = os.path.basename(file_path)
     filename = filename_zip.split('.zip')[0]
     manifest_file = "%s/xfdumanifest.xml" % filename
     unzip_command = "unzip %s %s" % (file_path, manifest_file)
     os.system(unzip_command)
+    return manifest_file
+
+def _get_S3_file_attributes(file_path):
+    filename_zip = os.path.basename(file_path)
+    filename = filename_zip.split('.zip')[0]
+    # TODO: Put Parsing in a general SAFE_Maniferst Parser module
+    manifest_file = _get_zip_manifest(file_path)
     tree = ET.parse(manifest_file)
     root = tree.getroot()
     metadataSection = getNodeByName(root, 'metadataSection')
