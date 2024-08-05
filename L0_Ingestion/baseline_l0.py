f4d865adf0de84c5ad73f30ecd0b7dc6e9dbed97
EO User <eouser@adg-rpp.novalocal>
2024-08-05 08:42:20 +0000

    Refactored L0 names ingestion: class querying LTA does not extract names; a separate class manages results from LTA; different handlers of results, that extract validities from them, are defined, based on missions; main procedure is modified accordingly

--- L0_Ingestion/baseline_l0.py
+++ L0_Ingestion/baseline_l0.py
@@ -1,13 +1,14 @@
 #!/bin/python
 # -*- coding: utf-8 -*-
 
-import psycopg2
 import argparse
-import requests
 import datetime as dt
 import time
-from time_formats import odata_datetime_format
+import sys
+import psycopg2
+import requests
 from requests.auth import HTTPBasicAuth
+from time_formats import odata_datetime_format
 from L0_Fields_parse import parse_start_stop_fields
 
 lta_baseurl = "aip.acri-st.fr"
@@ -15,9 +16,7 @@ lta_baseurl = "aip.acri-st.fr"
 # l0_product_type
 # S1_L0_types = ["RAW__0S"]
 # S3_L0_Types = ["MW_0_MWR___", "OL_0_EFR___", "SL_0_SLT___", "SR_0_SRA___"]
-class lta_l0_retriever:
-    #coreURL = f"{lta_baseurl}/odata/v1/Products?$filter=ContentDate/Start gt %04d-%02d-%02dT%02d:%02d:%02d.000000Z and ContentDate/Start lt %04d-%02d-%02dT23:59:59.999999Z and contains(Name,'_RAW__0S')&$top=200"
-    #coreURL = f"{lta_baseurl}/odata/v1/Products?$filter=ContentDate/Start gt %s and ContentDate/Start lt %s and startswith(Name, '%s') and contains(Name,'%s')&$top=200&$expand=Attributes"
+class LtaL0Retriever:
     nbRequestsMaxTries = 5
     headers = {'Content-type': 'application/json'}
     NO_EXP_CORE_URL = "{baseurl}/Products?$filter=ContentDate/Start gt %s and ContentDate/Start lt %s and startswith(Name, '%s') and contains(Name,'%s')&$top=200"
@@ -31,32 +30,32 @@ class lta_l0_retriever:
         self._num_days = int(num_days)
         self._now_time = dt.datetime.utcnow()
         self.coreURL = self._core_urls.get(mission).format(baseurl= lta_url)
-        self._l0_name_parser = L0_name_parser_factory.get_l0_name_parser(mission)
 
 
     # TODO: do we get from a time, or from the start of a day?
     def _get_names(self, unit, l0_type, from_date, to_date, req_timeout=30):
         # Build request replacing from_date, from_time and to_date
-        request = ""
         next_trial_time = 60 # seconds
-        names = []
+        results = []
         resp = None
         requestTriesLeft = self.nbRequestsMaxTries
 
         #Construction de la requête
         request = (self.coreURL + "&$count=true") % (from_date, to_date, unit, l0_type)
         print(request)
- 
+
         while ((resp is None) or (resp.status_code != 200)) and (requestTriesLeft > 0):
             ntrials = self.nbRequestsMaxTries - requestTriesLeft
-            if ntrials > 0: 
+            if ntrials > 0:
                 # Make another try, until max_retry reached
-                print("Resp ", "None" if resp is None else resp.status_code, ": Waiting for next retry ", ntrials * next_trial_time, " secs")
+                print("Resp ", "None" if resp is None else resp.status_code,
+                      ": Waiting for next retry ", ntrials * next_trial_time, " secs")
                 time.sleep(ntrials * next_trial_time)
             requestTriesLeft -= 1
             print('Sending request try : [%s/%s]' % (self.nbRequestsMaxTries - requestTriesLeft, self.nbRequestsMaxTries))
             try:
-                resp = requests.get(request, auth=self._authentication,headers=self.headers, timeout=req_timeout)
+                resp = requests.get(request, auth=self._authentication,
+                                    headers=self.headers, timeout=req_timeout)
             except requests.Timeout as to_exc:
                 print("Request  failed after timeout (", req_timeout,"): ", str(to_exc))
                 resp = None
@@ -66,10 +65,10 @@ class lta_l0_retriever:
         elif resp.status_code == 200:
             count = int(resp.json()["@odata.count"])
             nb_steps = int(count/200)
-                
+
             for aux in resp.json()["value"]:
                 print("Lta record: ", aux)
-                names.append(aux['Name'])
+                results.append(aux)
 
             for step in range( nb_steps ):
                 # On boucle sur toutes les pages de 200 fichiers L0
@@ -84,21 +83,21 @@ class lta_l0_retriever:
                 while ((resp is None) or (resp.status_code != 200)) and (requestTriesLeft > 0):
                     requestTriesLeft -= 1
                     print('Sending request try : [%s/%s]' % (self.nbRequestsMaxTries - requestTriesLeft, self.nbRequestsMaxTries))
-                    # TODO: Apply Timout handling also HERE: 
+                    # TODO: Apply Timout handling also HERE:
                     # Move to function Request execution and handling of response
                     resp = requests.get(request, auth=self._authentication,headers=self.headers)
 
                 if resp.status_code == 200:
                     for aux in resp.json()["value"]:
                         print("Lta record: ", aux)
-                        names.append(aux['Name'])
+                        results.append(aux)
                 else:
                     raise Exception("Bad return code ", resp.status_code, " for request: "+request)
         else:
             raise Exception("Bad return code ", resp.status_code, " for request: "+request)
-        return sorted(names)
+        return results
 
-    def get_lta_l0_names(self, unit_name, l0_type, from_date):
+    def get_lta_l0_products(self, unit_name, l0_type, from_date):
         # adjust the dates, by getting the minimum between today and the date + N days (end of day)
         to_date = from_date + dt.timedelta(days=self._num_days)
         end_time = min (to_date, self._now_time)
@@ -106,26 +105,38 @@ class lta_l0_retriever:
         # TBD: do we pass date objects, or strings? Better to leave to query function the format of the date string
         end_time_str =  dt.datetime.strftime(end_time, odata_datetime_format)
         from_date_str =  dt.datetime.strftime(from_date, odata_datetime_format)
-        return self._get_names(unit_name, l0_type, from_date_str, end_time_str)
+        lta_results = self._get_names(unit_name, l0_type, from_date_str, end_time_str)
+        return lta_results
 
-class L0_name_parser:
+    def get_lta_l0_names(self, unit_name, l0_type, from_date):
+        lta_results = self.get_lta_l0_products(unit_name, l0_type, from_date)
+        return [rec['Name'] for rec in lta_results]
+
+
+class L0_NameParser:
     def __init__(self, start_pos, field_len, unit_len):
-        print("Name parser: start: ", start_pos, ", field len: ", field_len, ", Unit field len: ", unit_len)
+        print("Name parser: start: ", start_pos,
+              ", field len: ", field_len, ", Unit field len: ", unit_len)
         self._start_pos = start_pos
         self._field_len = field_len
         self.unit_len = unit_len
 
     def get_start_stop(self, l0_name):
         print(l0_name)
-        start = l0_name[self._start_pos:self._start_pos+self._field_len]
-        start_pos = self._start_pos + self._field_len + 1
-        stop = l0_name[self._start_pos:self._start_pos+self._field_len]
-        return start, stop
+        field_start_pos = self._start_pos
+        field_end_pos = field_start_pos + self._field_len
+        start_time = l0_name[field_start_pos:field_end_pos]
+        # move to next field
+        field_start_pos = field_end_pos + 1
+        # field_start_pos += self._field_len + 1
+        field_end_pos = field_start_pos + self._field_len
+        stop_time = l0_name[field_start_pos:field_end_pos]
+        return start_time, stop_time
 
 class L0_name_parser_factory:
     mission_validity_parameters = {
         'S1': { 'start': 17, 'len': 15, 'unit_len': 3},
-        # 'S2': { 'start': 27, 'len': 15, 'unit_len': 3},
+        'S2': { 'start': 27, 'len': 15, 'unit_len': 3},
         'S3': { 'start': 16, 'len': 15, 'unit_len': 6},
     }
 
@@ -137,24 +148,82 @@ class L0_name_parser_factory:
         start = mission_parms.get('start')
         num_chars = mission_parms.get('len')
         unit_len = mission_parms.get('unit_len')
-        return L0_name_parser(start, num_chars, unit_len)
+        return L0_NameParser(start, num_chars, unit_len)
 
-class L0_names_loader:
+class L0_validity_factory:
+    @classmethod
+    def get_l0_validity_extractor(cls, mission):
+        if mission in ('S1', 'S3'):
+            return L0_validity_name_extractor(mission)
+        elif mission == 'S2':
+            return L0_validity_attributes_extractor(mission)
+        else:
+            raise Exception("Mission not supported")
+
+class L0_validity_extractor:
+    def __init__(self, mission):
+        self._mission = mission
+        self._l0_name_parser = L0_name_parser_factory.get_l0_name_parser(mission)
+
+    @property
+    def unit_len(self):
+        return self._l0_name_parser.unit_len
+
+    @property
+    def mission(self):
+        return self._mission
+
+    def get_product_validity(self, product_record):
+        raise Exception("Base Class method not callable")
+
+class L0_validity_attributes_extractor(L0_validity_extractor):
+    def __init__(self, mission):
+        L0_validity_extractor.__init__(self, mission)
+
+    def _getValidityFromAttributes(self, attributes):
+
+        validityDate = ''
+
+        for attributeDict in attributes:
+            if attributeDict['Name'] == 'productGroupId':
+                validityDate = attributeDict['Value'].split('_')[1]
+                break
+
+        return validityDate
+
+    # second type: read from record the L0 proudct name,
+    #      and attributes; retrieve from attributes the validity
+    def get_product_validity(self, product_record):
+        l0_name = product_record['Name']
+        start = stop = self._getValidityFromAttributes(product_record['Attributes'])
+        return l0_name, start, stop
+
+class L0_validity_name_extractor(L0_validity_extractor):
+    def __init__(self, mission):
+        L0_validity_extractor.__init__(self, mission)
+
+    # first type: read from record the L0 product name, extract validity from name
+    def get_product_validity(self, product_record):
+        l0_name = product_record['Name']
+        prod_start, prod_stop = self._l0_name_parser.get_start_stop(l0_name)
+        return l0_name, prod_start, prod_stop
+
+class L0_NamesLoader:
     _insert_sql = """INSERT INTO l0_products(name, validitystart, validitystop) VALUES(%s, %s, %s);"""
     #   We retrieve all the satellite/satellte_sensor for a mission
     _query_sql = """SELECT SUBSTRING(name, 0, %d) unit, MAX(validitystart) FROM l0_products where SUBSTRING(name, 0, 3) = '%s' group by unit;"""
-    def __init__(self, mission, host, port, database, user, password):
+    def __init__(self, mission, dbconn):
         print("Initializing L0 Loader for mission", mission)
-        self._l0_name_parser = L0_name_parser_factory.get_l0_name_parser(mission)
-        self._db_conn = psycopg2.connect(host=host,port=port,database=database,user=user,password=password)
+        self._db_conn = dbconn 
+        self._l0_validity_extractor = L0_validity_factory.get_l0_validity_extractor(mission)
         self._mission = mission
 
     def get_l0_latest_validity(self):
         type_last_val_dict = {}
         with self._db_conn as conn:
-            with self._db_conn.cursor() as cursor:
-                print("Executing ", self._query_sql % (self._l0_name_parser.unit_len + 1, self._mission))
-                cursor.execute(self._query_sql % (self._l0_name_parser.unit_len + 1, self._mission))
+            with conn.cursor() as cursor:
+                print("Executing ", self._query_sql % (self._l0_validity_extractor.unit_len + 1, self._mission))
+                cursor.execute(self._query_sql % (self._l0_validity_extractor.unit_len + 1, self._mission))
                 results = cursor.fetchall()
                 print("Query returned: ", results)
                 # each record in result contains: unit, date
@@ -164,21 +233,25 @@ class L0_names_loader:
         # return a dict unit/lastest_date
         return type_last_val_dict
 
-    def add_l0_names(self, l0_names):
+    def _get_lta_l0_validities(self, l0_products):
+        # Appliy validity extractor to LTA query results
+        return [self._l0_validity_extractor.get_product_validity(l0_product)
+                for l0_product in l0_products]
+
+    def add_l0_name_validities(self, l0_validities):
         with self._db_conn as conn:
             with conn.cursor() as cursor:
-                for l0_name in l0_names:
-                    start, stop = self._l0_name_parser.get_start_stop(l0_name)
+                for l0_record in l0_validities:
+                    l0_name, start, stop = l0_record
                     try:
+                        print("Executing ", self._insert_sql % (l0_name, start, stop))
                         cursor.execute(self._insert_sql, (l0_name, start, stop))
                         conn.commit()
                     except Exception as e:
                         print(e)
                         conn.rollback()
 
-    def add_l0_name(self, l0_name):
-        # S1B_IW_RAW__0SDV_20190822T145111_20190822T145143_017700_0214CF_EBA5.SAFE.zip
-        start, stop = self._l0_name_parser.get_start_stop(l0_name)
+    def add_l0_name(self, l0_name, start, stop):
         with self._db_conn as conn:
             with conn.cursor() as cursor:
 
@@ -188,11 +261,13 @@ class L0_names_loader:
                 except Exception as e:
                     print(e)
                     conn.rollback()
+    def add_l0_products(self, l0_products):
+        l0_validities = self._get_lta_l0_validities(l0_products)
+        self.add_l0_name_validities(l0_validities)
 
 
-        
-if __name__ == "__main__": 
-
+def get_command_arguments():
+    print("Called with command line: ", sys.argv)
     parser = argparse.ArgumentParser(description="This script allows you to upload to the Task 3 a listing of L0 files for Mission",  # main description for help
             epilog='\n\n', formatter_class=argparse.RawTextHelpFormatter)  # displayed after help
     parser.add_argument("-m", "--mission",
@@ -228,18 +303,22 @@ if __name__ == "__main__":
     parser.add_argument("-pw", "--ltapassword",
                         help="Prip password ",
                         required=False)
+    arg_values = parser.parse_args()
+    print("Command line arguments: ", arg_values)
+    if arg_values.inputFile is None and (arg_values.ltaurl is  None or arg_values.ltauser is None or arg_values.ltapassword is None):
+         parser.error("LTA arguments shall be all specified, if you are not using input File")
+    return arg_values
 
-    args = parser.parse_args()
 
-    print("arguments: ", args)
+if __name__ == "__main__":
+
+    args = get_command_arguments()
     host=args.host
     port=args.port
     database=args.dbName
     user=args.user
     password=args.password
     mission=args.mission
-    if args.inputFile is None and (args.ltaurl is  None or args.ltauser is None or args.ltapassword is None):
-         parser.error("LTA arguments shall be all specified, if you are not using input File")
 
     _default_num_days = 15
     num_days = args.numDays if args.numDays is not None else _default_num_days
@@ -249,36 +328,41 @@ if __name__ == "__main__":
         'S3':  ["MW_0_MWR___", "OL_0_EFR___", "SL_0_SLT___", "SR_0_SRA___"]
     }
     try:
-        l0_loader = L0_names_loader(mission, host, port, database, user, password) 
+        rdb_conn = psycopg2.connect(host=host, port=port, database=database,
+                                    user=user, password=password)
+        l0_loader = L0_NamesLoader(mission, rdb_conn)
 
         l0_list_file = args.inputFile
 
-        # if input file is defined,get from input file 
+        # if input file is defined,get from input file
+        # get validities from input file!!
+        # Name, start, stop
+        # in csv format
         if l0_list_file is not None:
             with open(l0_list_file, "r") as fid:
                 for line in fid.readlines():
-                    l0_name = line.rstrip('\n')
-                    l0_loader.add_l0_name(l0_name)
+                    l0_validities = line.rstrip('\n').split(' ')
+                    # S1B_IW_RAW__0SDV_20190822T145111_20190822T145143_017700_0214CF_EBA5.SAFE.zip
+                    # start, stop = self._l0_name_parser.get_start_stop(l0_name)
+                    l0_name, start, stop = l0_validities
+                    l0_loader.add_l0_name(l0_name, start, stop)
 
         else:
             # Get Max validity for all types related to mission
             for l0_type in mission_l0_types[mission]:
-                last_l0_type_table = l0_loader.get_l0_latest_validity() 
+                last_l0_type_table = l0_loader.get_l0_latest_validity()
                 print("Last L0 validity for each Type for mission ", mission, ": ", last_l0_type_table)
-                lta_retriever = lta_l0_retriever(mission, args.ltaurl, args.ltauser, args.ltapassword, num_days)
+                lta_retriever = LtaL0Retriever(mission, args.ltaurl, args.ltauser, args.ltapassword, num_days)
                 # for each satellite, or satellite_sensoor:
                 for unit, val_time in last_l0_type_table.items():
                     # Get from LTA a list of L0 names, for N days (or up to today),
-                    unit_l0_names = lta_retriever.get_lta_l0_names(unit, l0_type, val_time)
-                    print("Retrieved L0 names for unit: ", unit, ": ", unit_l0_names)
+                    l0_products = lta_retriever.get_lta_l0_products(unit, l0_type, val_time)
+                    print("Retrieved L0 names for unit: ", unit, ": ", (prod['Name'] for prod in l0_products))
                     # With N from command line, with default if not specified (or default got from config file)
                     # Add the l0 names to db
-                    l0_loader.add_l0_names(unit_l0_names)
-
+                    l0_loader.add_l0_products(l0_products)
 
     except Exception as e:
         print(e)
         # if conn:
         #     conn.rollback()
-
-
