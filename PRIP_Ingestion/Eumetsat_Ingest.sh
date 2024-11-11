ffe079459f7ff366bf90132b44ac9539e69f0a99
EO User <eouser@adg-rpp.novalocal>
2024-11-11 10:11:26 +0000

    Scripts to controll batch ingestion of products from Eumetsat FTP; separate script to hold functions to manage a Last Date ingested file

---
+++ PRIP_Ingestion/Eumetsat_Ingest.sh
@@ -0,0 +1,134 @@
+#!/bin/bash 
+
+source InitFuncs.sh
+source FilesUtils.sh
+source IngestDownloaded_funcs.sh
+
+
+# TODO: pass FTP Access variables + START_FOLDER
+function ingest_day_aux_files() {
+
+  # Expecting argument for MISSION to be ingested
+  #  From/TO date arguments (with option included) are defined
+  #   using global variables
+  if [[ $# != 4 ]] ; then
+      echo "ingest_from_file function arguments: TEMP_FOLDER MISSION AUX_TYPE DAY "
+      echo "Received: $@"
+      exit 1
+  fi
+  TEMP_ROOT=$1
+  MISSION=$2
+  AUX_TYPE=$3
+  DAY=$4
+  # Change format of DAY from YYYY-MM-DD to YYYY/MM/DD
+  MISSION_TEMP_ROOT=${TEMP_ROOT}/$AUX_TYPE/$DAY
+  MISSION_TEMP_FOLDER=${MISSION_TEMP_ROOT}/DOWNLOAD
+  MISSION_TEMP_FOLDER_LISTING=${MISSION_TEMP_ROOT}/FILES_LISTING
+  MISSION_TEMP_FOLDER_JSONS=${MISSION_TEMP_ROOT}/BASELINE_JSONS
+  create_folder $MISSION_TEMP_FOLDER
+  create_folder $MISSION_TEMP_FOLDER/${MISSION}
+  create_folder $MISSION_TEMP_FOLDER_LISTING
+  create_folder $MISSION_TEMP_FOLDER_JSONS
+    echo "[BEGIN] Ingesting Mission ${MISSION} , Aux Type : : $AUX_TYPE for day $DAY"
+    echo "[BEGIN] Ingesting Mission ${MISSION} , Aux Type : : $AUX_TYPE for day $DAY" >> ${ERROR_FILE_LOG}
+  # TODO: python script is not using a TEMP FOLDER!!!
+  python3 -u ${CUR_DIR}/eumetsat_provider/Eumetsat_ingest_products.py  -u ${FTP_USER} -pw ${FTP_PASS} \
+        -w ${MISSION_TEMP_FOLDER}/${MISSION} \
+        -fh ${FTP_HOST} \
+        -sf ${FTP_FOLDER} \
+        -d $DAY
+  code=$?
+  if [ $code -ne 0 ]; then
+    echo "PRIP Retrieve failed"
+    echo "PRIP Retrieve failed" >> ${ERROR_FILE_LOG}
+  fi
+  ingest_downloaded_files $MISSION $code "$MISSION_TEMP_FOLDER" "${MISSION_TEMP_FOLDER_LISTING}" "${MISSION_TEMP_FOLDER_JSONS}"
+  ingestion_code=$?
+  master_code=$ingestion_code
+  if [ $ingestion_code -eq 0 ]; then
+  # AUXIP Ingested Files: Generation of related JSONs to put in ReproBase
+   ingest_reprobase $MISSION $ingestion_code "${MISSION_TEMP_FOLDER_LISTING}"  "${MISSION_TEMP_FOLDER_JSONS}"
+   code=$?
+   master_code_reprobase=$code
+    master_code=$master_code_reprobase
+  fi
+  echo "Removing daily temporary folders"
+  if [ $master_code -eq 0 ]; then
+      echo "No errors for $MISSION ingestion"
+      echo "Removing temporary folders"
+      echo "Removing TEMP Folder ${MISSION_TEMP_FOLDER:?}"
+      rm -rf "${MISSION_TEMP_FOLDER:?}"/
+      REM_RES=$?
+      echo "Removal result: $REM_RES"
+      echo "Removing TEMP LISTING Folder ${MISSION_TEMP_FOLDER_LISTING:?}"
+      rm -rf ${MISSION_TEMP_FOLDER_LISTING:?}/
+      REM_RES=$?
+      echo "Removal result: $REM_RES"
+      echo "Removing TEMP Reprobase JSON Folder ${MISSION_TEMP_FOLDER_JSONS:?}"
+      rm -rf ${MISSION_TEMP_FOLDER_JSONS:?}/
+      REM_RES=$?
+      echo "Removal result: $REM_RES"
+
+    else
+      echo "Ingestion failure - Keeping Temporary files "
+    fi
+    echo "[END] Completed Ingestion of Mission ${MISSION} "
+    echo "[END] Completed Ingestion of Mission ${MISSION} " >> ${ERROR_FILE_LOG}
+  ingestion_code=$?
+  echo "Removing temporary folders"
+  return $master_code
+}
+
+
+
+
+function ingest_mission_date_interval() {
+#   Input arguments:
+#  TEMP_FOLDER MISSION AUX_TYPE FROM_DATE NUM_DAYS 
+# Loop: set CURR_DATE = START_DATE
+
+   local WORK_FOLDER=$1
+   local MISSION=$2
+   local AUX_TYPE=$3
+   local CURR_DATE=$4
+
+   #EXECUTION DATE
+   EXEC_DATE=$(date '+%Y-%m-%d')
+
+   # MOVE Folders creation inside ingest mission, to be done for each BATCH
+   init_folders $EXEC_DATE
+
+   NLOOPS=$5
+   echo "Mission $MISSION: Executing $NLOOPS one day ingestions from date $CURR_DATE"
+   # Loop N times 
+    for in in $(seq $NLOOPS); do 
+        echo "Starting ingestion for date $CURR_DATE"
+        ingest_day_aux_files $TEMP_FOLDER $MISSION $AUX_TYPE $CURR_DATE 
+# INCREMENT curr_date
+        CURR_DATE=$(date -d "${CURR_DATE} +1 days" +${DATE_FORMAT})
+    done
+
+ingestion_code=0
+# Remove temporary folders if ALL missions have been successful
+#  if [ $ingestion_code -eq 0 ]; then
+#    echo "No Errors Found"
+#    echo "Removing TEMP Folder ${TEMP_FOLDER:?}"
+#    rm -rf "${TEMP_FOLDER:?}"/
+#    echo "Removing TEMP LISTING Folder ${TEMP_FOLDER_LISTING:?}"
+#    rm -rf ${TEMP_FOLDER_LISTING:?}/
+#    echo "Removing TEMP Reprobase JSON Folder ${TEMP_FOLDER_JSONS:?}"
+#    rm -rf ${TEMP_FOLDER_JSONS:?}/
+#  else
+#     echo "Errors found during ingestion - Keeping temporary folders"
+#  fi
+
+echo "Done"
+
+# Removing the error log if the number of lines equals 3 : one line per tmp directories created at the beginning
+if [ "$( wc -l < ${ERROR_FILE_LOG} )" -eq 3 ]; then
+  echo "No errors during ingestion : deleting error file"
+  rm $ERROR_FILE_LOG
+fi
+}
+
+
---
+++ PRIP_Ingestion/LastDateFile.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+
+DATE_FORMAT="%Y-%m-%d"
+function get_date_file_name() {
+MISS=$1
+ROOT_F=$2
+if [[ -z ${ROOT_F} ]]; then
+   ROOT_F=${CUR_DIR}
+fi
+echo "${ROOT_F}/${MISS}_AlignLastDate.txt"
+}
+function read_start_date() {
+MISSION=$1
+ROOT_F=$2
+  DATE_FILE=$(get_date_file_name $MISSION ${ROOT_F})
+  if [ -f ${DATE_FILE} ]; then
+    # Read Date from file
+    echo "Align Last Date file found" >&2
+    read -r LAST_DATE < ${DATE_FILE}
+    if [ -z ${LAST_DATE} ]; then
+       echo "No date set in Last Date File" >&2
+       exit -1
+    else
+      # Redirect command to avoid mixing output with this function otuput
+      if   date -d $LAST_DATE +${DATE_FORMAT} >&2
+      then
+         #START_DATE=$(date -d "$LAST_DATE +1 days" +${DATE_FORMAT} )
+         START_DATE=$LAST_DATE
+      else
+        echo "Date in Last Date file has wrong format" >&2
+        exit -1
+      fi
+    fi
+  else
+    # Read Date from Env Variable
+    echo "Align Last Date file ${DATE_FILE} not found" >&2
+    if [ -z "${INGESTION_START}" ]; then
+      echo "INGESTION_START not set" >&2
+      exit -1
+    fi
+  
+    START_DATE=${INGESTION_START}
+    echo "Env Var: $START_DATE" >&2
+    if  ! date -d $START_DATE +${DATE_FORMAT} >&2
+    then
+      echo "Environment Variable L0_ALIGN_Start has wrong date format" >&2
+      exit -1
+    fi
+  fi
+  echo "Returning start date: $START_DATE - " >&2
+  # Print result
+  echo  "$START_DATE"
+}
+
+function update_last_date() {
+LAST_DATE=$1
+MISSION=$2
+TARGET_F=$3
+  DATE_FILE=$(get_date_file_name $MISSION ${TARGET_F})
+echo ${LAST_DATE} > $DATE_FILE
+}
+
