cd8ee61584446408915910c9fe02978d523bb991
EO User <eouser@adg-rpp.novalocal>
2025-02-17 21:03:40 +0000

    Module with function to extract file names from input file

---
+++ PRIP_Ingestion/ingestion/AuxFilesListUtils.py
@@ -0,0 +1,8 @@
+
+
+def get_aux_names_from_file(filepath):
+    with open(filepath) as f:
+        names = f.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    names = [x.strip() for x in names if len(x)] 
+    return names
