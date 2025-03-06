a7309406939943dd081dbc3204d9e12677026121
EO User <eouser@adg-rpp.novalocal>
2025-03-06 16:18:51 +0000

    Sql Script to update S1 L2OCN baseline

---
+++ Utilities/S1_Baseline_configuration/S1_Baseline_L2OCN_AddL1_AuxTypes.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.auxfiletypes_productlevels(auxfiletypes_longname, productlevels_level) VALUES('AUX_RESATT', 'L1');
+INSERT INTO public.auxfiletypes_producttypes(auxfiletypes_longname, producttypes_type) VALUES ('AUX_RESATT', 'L2OCN');
+INSERT INTO public.auxfiletypes_productlevels(auxfiletypes_longname, productlevels_level) VALUES('AUX_CAL', 'L1');
+INSERT INTO public.auxfiletypes_producttypes(auxfiletypes_longname, producttypes_type) VALUES ('AUX_CAL', 'L2OCN');
+INSERT INTO public.auxfiletypes_productlevels(auxfiletypes_longname, productlevels_level) VALUES('AUX_INS', 'L1');
+INSERT INTO public.auxfiletypes_producttypes(auxfiletypes_longname, producttypes_type) VALUES ('AUX_INS', 'L2OCN');
+INSERT INTO public.auxfiletypes_productlevels(auxfiletypes_longname, productlevels_level) VALUES('AUX_POEORB_S1', 'L1');
+INSERT INTO public.auxfiletypes_producttypes(auxfiletypes_longname, producttypes_type) VALUES ('AUX_POEORB_S1', 'L2OCN');
+INSERT INTO public.auxfiletypes_productlevels(auxfiletypes_longname, productlevels_level) VALUES('AUX_PP1', 'L1');
+INSERT INTO public.auxfiletypes_producttypes(auxfiletypes_longname, producttypes_type) VALUES ('AUX_PP1', 'L2OCN');
+INSERT INTO public.auxfiletypes_productlevels(auxfiletypes_longname, productlevels_level) VALUES('AUX_PREORB_S1', 'L1');
+INSERT INTO public.auxfiletypes_producttypes(auxfiletypes_longname, producttypes_type) VALUES ('AUX_PREORB_S1', 'L2OCN');
+INSERT INTO public.auxfiletypes_productlevels(auxfiletypes_longname, productlevels_level) VALUES('AUX_RESORB_S1', 'L1');
+INSERT INTO public.auxfiletypes_producttypes(auxfiletypes_longname, producttypes_type) VALUES ('AUX_RESORB_S1', 'L2OCN');
+
